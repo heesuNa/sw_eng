105b08728d5e377d2906e8795388edf3106df249
leeseulkim <dltmfdltmf00@naver.com>
2015-11-01 14:40:40 +0900

    student.java는 학생정보 클래스이고, studentMain.java는 학생정보 클래스를 이용하여 추가, 삭제, 보기, 수정, 저장하는 것

---
+++ Student.java
@@ -0,0 +1,54 @@
+package student_information;
+
+import java.io.Serializable;
+
+public class Student implements Serializable
+{     
+	String id;
+	String name;
+	String department;
+	String phoneNumber;
+
+   public Student(String id, String name, String department, String phoneNumber)
+   {
+	   this.id=id;
+	   this.name=name;
+	   this.department=department;
+	   this.phoneNumber=phoneNumber;
+	}
+   
+	public String toString()
+	{
+		String description;
+		description="-학번 : "+id +"\n";
+		description+="-이름: "+name+"\n";
+		description+="-학과: "+department+"\n";
+		description+="-핸드폰 번호: "+phoneNumber+"\n";
+		
+		return description;
+	}
+
+	
+	public String getId()
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getDepartment()
+	{
+		return department;
+	}
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+
+
+}
+
+
+	
